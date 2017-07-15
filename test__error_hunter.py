e0fc232d6a2ed662273523b87ac3b3372a253532
aploium <i@z.codes>
2017-07-15 23:14:18 +0800

    commit

--- test__error_hunter.py
+++ test__error_hunter.py
@@ -1,39 +1,36 @@
 #!/usr/bin/env python3
 # coding=utf-8
-
-import logging
-import logging.handlers
+import requests
 import err_hunter
 
-FORMAT = "[%(levelname)s %(asctime)s %(module)s.%(funcName)s#%(lineno)d] %(message)s"
-logging.basicConfig(
-    format=FORMAT,
-    level=logging.DEBUG,
-)
 
-err_hunter.apply_handler("https://api.dyn.li/log", method="POST")
+#
+# FORMAT = "[%(levelname)s %(asctime)s %(module)s.%(funcName)s#%(lineno)d] %(message)s"
+# logging.basicConfig(
+#     format=FORMAT,
+#     level=logging.DEBUG,
+# )
+#
+# log = logging.getLogger(__name__)
+#
+# log.info("info")
 
-log = logging.getLogger(__name__)
 
-log.info("info")
-
-
-def func3():
+def error_func():
     monkey = 3
     a = 1 / 0
-
-
-def func2():
-    dog = 1
-    func3()
+    universe = 42
 
 
 def func():
-    local_var = 23333
-    log.error("hello world", extra={"cat": 1})
+    cat = 7
+    r = requests.get("http://example.com")
+    error_func()
+    monkey = 7
 
 
+some_global_var = {"a": "b"}
 try:
-    func2()
-except:
     func()
+except:
+    err_hunter.print_exc()
